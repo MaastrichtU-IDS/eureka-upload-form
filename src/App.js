1212d885b4e87e3ef837dfd0e44e7e2581a579b4
thendriks <t.hendriks@maastrichtuniversity.nl>
2021-06-14 12:38:06 +0200

    Added validation.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,197 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from "@rjsf/material-ui";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  function transformErrors(errors) {
+    return errors.map(error => {
+      console.log(error.name);
+      if (error.name === "enum") {
+        error.message = ""
+      }
+      if (error.name === "required") {
+        error.message = "Please provide a value for this field"
+      }
+      if (error.name === "oneOf") {
+        error.message = ""
+      }
+      return error;
+    });
+  }
+
+  function validate(formData, errors) {
+  // if (formData.pass1 !== formData.pass2) {
+  //   errors.pass2.addError("This field is required");
+  // }
+  return errors;
+}
 
   const schema = {
     "title": "EUREKA Knowledge Object Upload Form",
-    "description": "Single object upload form for the EUREKA Farmbook. For large automated large scale uploads of already structured metadata, please contact ...",
+    "description": "Single object upload form for the EUREKA Farmbook. For large automated large scale uploads of already structured metadata, please contact us on the #eureka-development slack channel.",
     "type": "object",
     "required": [
-      "Category",
+      "Knowledge Object Location",
       "Title",
       "Description",
+      "Project Name",
+      "Project Acronym",
+      "Project URL",
+      "Category",
       "Keywords",
-      "Creators"
+      "Creators",
+      "Languages",
+      "Date of Creation",
+      "PotentialAction",
+      "Place(s)"
     ],
+    "definitions": {
+      "license":{
+        "type": "object",
+        "properties": {
+          "Under which license will the knowledge object be made available?": {
+            "type": "string",
+            "enum": ["CC BY",
+            "CC BY-SA",
+            "CC BY-NC",
+            "CC BY-NC-SA",
+            "CC BY-ND",
+            "CC BY-NC-ND", 
+            "Other"]
+            ,
+            "default": "CC BY"
+          }
+        },
+        "required": [
+          "Under which license will the knowledge object be made available?"
+        ],
+        "dependencies": {
+          "Under which license will the knowledge object be made available?": {
+            "oneOf": [
+              {
+                "properties": {
+                  "Under which license will the knowledge object be made available?": {
+                    "enum": [
+                      "Other"
+                    ]
+                  },
+                  "License URL": {
+                    "type": "string",
+                    "title": "Please provide the URL to the license"
+                  }
+                },
+                "required": [
+                  "License URL"
+                ],
+              },
+              {
+                "properties": {
+                  "Under which license will the knowledge object be made available?": {
+                    "enum": [
+                    "CC BY",
+                    "CC BY-SA",
+                    "CC BY-NC",
+                    "CC BY-NC-SA",
+                    "CC BY-ND",
+                    "CC BY-NC-ND", ]
+                  }
+                }
+              }
+            ]
+          }
+        }
+      },
+      "location": {
+        "title": "Location",
+        "type": "object",
+        "properties": {
+          "Is your knowledge object available online?": {
+            "type": "string",
+            "enum": [
+              "Yes, I will provide the URL to the object",
+              "No, I will upload the object from my local drive",
+            ],
+            "default": "Yes, I will provide the URL to the object"
+          }
+        },
+        "required": [
+          "Is your knowledge object available online?"
+        ],
+        "dependencies": {
+          "Is your knowledge object available online?": {
+            "oneOf": [
+              {
+                "properties": {
+                  "Is your knowledge object available online?": {
+                    "enum": [
+                      "No, I will upload the object from my local drive"
+                    ]
+                  },
+                  "File": {
+                    "type": "string",
+                    "format": "data-url",
+                    "title": "the file to upload as a knowledge object"
+                  }
+                },
+                "required": [
+                  "File"
+                ],
+              },
+              {
+                "properties": {
+                  "Is your knowledge object available online?": {
+                    "enum": [
+                      "Yes, I will provide the URL to the object"
+                    ]
+                  },
+                  "Knowledge Object URL": {
+                    "type": "string",
+                    "description": "the URL to the knowledge object"
+                  }
+                },
+                "required": [
+                  "Knowledge Object URL"
+                ]
+              }
+            ]
+          }
+        }
+      }
+    },
     "properties": {
+      "Knowledge Object Location": {
+        "title": "Knowledge Object Location",
+        "$ref": "#/definitions/location"
+      },
+      "Title": {
+        "type": "string",
+        "title": "Title",
+        "description": "The title of the knowledge object",
+      },
+      "Description": {
+        "type": "string",
+        "title": "Description",
+        "description": "A short (500 character or around 100 words) summary of the knowledge object."
+      },
       "Project Name": {
         "type": "string",
-        "title": "The project under which the knowledge object was created.",
-        "default": ""
+        "title": "Project Name",
+        "description": "The project under which the knowledge object was created"
       },
       "Project Acronym": {
         "type": "string",
-        "title": "The acronym of the project under which the knowledge object was created.",
-        "default": ""
+        "title": "Project Acronym",
+        "description": "The acronym of the project under which the knowledge object was created"
       },
       "Project URL": {
         "type": "string",
-        "title": "The main URL of the project under which the knowledge object was created.",
-        "default": ""
-      },
-      "Knowledge Object URL": {
-        "type": "string",
-        "title": "Please provide the URL to the knowledge object.",
-        "default": ""
-      },
-      "Knowledge Object URL": {
-        "type": "string",
-        "title": "Please provide the URL to the knowledge object.",
-        "default": ""
-      },
-      "Knowledge Object URL": {
-        "type": "string",
-        "title": "Please provide the URL to the knowledge object.",
-        "default": ""
+        "title": "Project URL",
+        "description": "The main URL of the project under which the knowledge object was created"
       },
       "Category": {
         "type": "string",
         "title": "Category",
+        "description": "The category that best fits the type of the supplied knowledge object",
         "enum": ["Document",
         "Spreadsheet",
         "Presentation",
@@ -60,76 +202,80 @@ function App() {
         "Dataset"
         ]
       },
-      "Title": {
-        "type": "string",
-        "title": "Please provide the title of the knowledge object.",
-        "default": ""
-      },
-      "Description": {
-        "type": "string",
-        "title": "Please provide a summary of the knowledge object.",
-        "default": ""
-      },
       "Keywords": {
         "title": "Keywords",
+        "description": "Please provide a list of keywords indicative of the content of the object",
         "type": "array",
         "items": {
           "type": "string"
         }
       },
       "Creators": {
-        "title": "Creator(s)",
+        "title": "Creators",
+        "description": "Provide the name(s) of the creator(s) of the object",
         "type": "array",
         "items": {
           "type": "string"
         }
       },
       "Languages": {
-        "title": "Language(s)",
+        "title": "Languages",
+        "description": "Please provide the language(s) in which the knowledge object is available. In case translations of the object are seperate files or pages, please handle each translation as it's own object.",
         "type": "array",
         "items": 
           {
             "title": "Language",
             "type": "string",
             "default": "English",
-            "enum": ["Dutch",
-                    "English"
-            ]
+            "enum": ["Bulgarian",
+            "Croatian",
+            "Czech",
+            "Danish",
+            "Dutch",
+            "English",
+            "Estonian",
+            "Finnish",
+            "French",
+            "German",
+            "Greek",
+            "Hungarian",
+            "Irish",
+            "Italian",
+            "Latvian",
+            "Lithuanian",
+            "Maltese",
+            "Polish",
+            "Portuguese",
+            "Romanian",
+            "Slovak",
+            "Slovene",
+            "Spanish",
+            "Swedish"]
           }
       },
-      "CreationDate": {
+      "Date of Creation": {
         "title": "Date of Creation",
-        "description": "The date on which the knowledge object was created.",
+        "description": "The date on which the knowledge object was created",
         "type": "object",
         "properties": {
           "date": {
             "type": "string",
             "format": "date"
           }
-        }
-      },
-      "CreationDate": {
-        "title": "Date of Creation",
-        "description": "The date on which the knowledge object was created.",
-        "type": "object",
-        "properties": {
-          "date": {
-            "type": "string",
-            "format": "date"
-          }
-        }
+        },
+        "required": [
+          "date"
+        ],
       },
       "License": {
-        "type": "string",
-        "title": "License",
-        "description": "The license under which the object will be shared.",
+        "title": "Knowledge Object Location",
+        "$ref": "#/definitions/license"
       },
       "Type": {
         "title": "Type",
         "type": "array",
         "items": 
           {
-            "title": "Language",
             "type": "string",
             "default": "English",
             "enum": ["article in conference proceedings",
@@ -164,22 +310,44 @@ function App() {
                 "Dissemination",
                 "Education/Training"]
       },
-      "Subject": {
-        "type": "string",
-        "title": "Subject",
-        "enum": ["Access to Data",
-                "Communication",
-                "Dissemination",
-                "Education/Training"]
-      },
-      "Place": {
-        "type": "string",
-        "title": "Subject",
-        "enum": ["Access to Data",
-                "Communication",
-                "Dissemination",
-                "Education/Training"]
-      },
+      "Place(s)": {
+        "title": "Place(s)",
+        "description": "Please select all European countries referenced in the knowledge object's contents.",
+        "type": "array",
+        "items": 
+          {
+            "title": "Language",
+            "type": "string",
+            "default": "English",
+            "enum": ["Austria",
+                       "Belgium",
+                       "Bulgaria",
+                       "Croatia",
+                       "Cyprus",
+                       "Czechia",
+                       "Denmark",
+                       "Estonia",
+                       "Finland",
+                       "France",
+                       "Germany",
+                       "Greece",
+                       "Hungary",
+                       "Ireland",
+                       "Italy",
+                       "Latvia",
+                       "Lithuania",
+                       "Luxembourg",
+                       "Malta",
+                       "Netherlands",
+                       "Poland",
+                       "Portugal",
+                       "Romania",
+                       "Slovakia",
+                       "Slovenia",
+                       "Spain",
+                       "Sweden"]
+          }
+      }
     }
   };
   const uiSchema = {
@@ -196,18 +364,15 @@ function App() {
         "orderable": false
       }
     },
+    "fixedArrayOfConditionals": {
+      "items": {
+        "Do you want to get rid of any?": {
+          "ui:widget": "radio"
+        }
+      }
+    }
   };
   const formData = {
-    "boolean": {
-      "default": true,
-      "radio": true,
-      "select": true
-    },
-    "firstName": "Chuck",
-    "lastName": "Norris",
-    "age": 75,
-    "bio": "Roundhouse kicking asses since 1940",
-    "password": "noneed"
   };
 
   const log = (type) => console.log.bind(console, type);
@@ -219,6 +384,10 @@ function App() {
             onChange={log("changed")}
             onSubmit={log("submitted")}
             onError={log("errors")}
+            showErrorList={false}
+            validate={validate}
+            liveValidate
+            transformErrors={transformErrors}
             />
     </div>
   );
